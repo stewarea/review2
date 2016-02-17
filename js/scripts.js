cc0a767ff5651c8699ac71081899fb40a0f7ca4d
Evan Stewart <Evan Stewart>
2016-02-16 21:48:06 -0800

    add hide so only one option appears on each click

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
-  $("form#match").submit(function(event) {
+  $("form#match button").click(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
-    var meal = $("select#meal").val()
-    var activity = $("select#activity").val()
-    var beach = $("select#beach").val()
+    var meal = $("select#meal").val();
+    var activity = $("select#activity").val();
+    var beach = $("select#beach").val();
 
 
-    if ((meal === "crab") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
-      $('#alaska').show();
-      }
-
-    if ((meal === "broth") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
-      $('#tikal').show();
-      }
-
-    if ((meal === "beans") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
-      $('#vietnam').show();
-      }
+    $(".result").hide();
 
     if ((gender === "null") || (meal === "null") || (activity === "null") || (beach === "null")) {
-        alert("Please fill out all of the questions.")
+      alert("Please fill out all of the questions.");
+    }
+    else {
+      if ((meal === "crab") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
+        $('#alaska').show();
         }
+
+      if ((meal === "broth") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
+        $('#tikal').show();
+        }
+
+      if ((meal === "beans") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
+        $('#vietnam').show();
+        }
+    }
 
      event.preventDefault();
   });
