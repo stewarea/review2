582208c62577e18a1b1d6c189f1b3df4999f906f
Evan Stewart <Evan Stewart>
2016-02-18 18:59:30 -0800

    fix form from disappearing when alert pops

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $("#again").hide();
   $("form#match button").click(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
@@ -8,27 +9,31 @@ $(document).ready(function() {
 
 
     $(".result").hide();
-    $('#match').hide();
+
     if ((gender === "null") || (meal === "null") || (activity === "null") || (beach === "null")) {
       alert("Please fill out all of the questions.");
-        ("form.match").show();
+        $("form.match").show();
+          $("#again").hide();
     }
     else {
+      $('#match').hide();
+      $("#again").show();
       if ((meal === "crab") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
         $('#alaska').show();
         }
 
-      if ((meal === "broth") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
+      else if ((meal === "broth") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
         $('#tikal').show();
         }
-
-      if ((meal === "beans") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
+      else {
+      // if ((meal === "beans") && (activity === "hiking" || "museum" || "eating") && (beach === "dig" || "relax" || "sand")) {
         $('#vietnam').show();
         }
     }
-    $(".result button").click(function(event) {
+    $("#again").click(function(event) {
       $(".result").hide();
         $("form#match").show();
+          $("#again").hide();
 
     });
      event.preventDefault();
